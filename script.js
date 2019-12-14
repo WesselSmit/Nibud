42cf044ddcd00a3ffb1ce1aa6fc301b9780b992e
Wessel Smit <wrsmit00@gmail.com>
2019-12-14 20:01:51 +0100

    progression bar quick fix

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+//form progression bar
 let allInputs = d3.selectAll('input')._groups[0]
 
-function update() {
+document.querySelectorAll('input').forEach(input => input.addEventListener('input', function () {
     let inputWithValue = 0
     allInputs.forEach(input => {
         if (input.value != "") {
@@ -8,9 +9,5 @@ function update() {
         }
     });
     let progression = document.querySelector('#progression').parentElement.getBoundingClientRect().width / allInputs.length * inputWithValue
-
     document.querySelector('#progression').style.paddingRight = progression + "px"
-    document.querySelector('#progression').textContent = inputWithValue / allInputs.length * 100 + "%"
-}
-
-document.querySelectorAll('input').forEach(input => input.addEventListener('input', () => update()))
+}))
