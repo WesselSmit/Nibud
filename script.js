0ff3adc8c4e0e1e1cc8f52dab2d1f69683d3bc14
Sjors <sjorspieter@live.nl>
2019-12-19 11:46:55 +0100

    new dataset

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ import transform from './modules/transformData.js'
 
 getData()
     .then(string => transform.createIndividualObjects(string))
+    .then(data => transform.createArray(data))
     .then(data => console.log(data))
     .catch(err => console.log(err))
-
 
 //TODO:
 //csv heeft lege waardes niet vervangen, dit resulteert in ;;
