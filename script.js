921f23220ca2b07435846cb56ba6ccc3c2cd2b65
Wessel Smit <wrsmit00@gmail.com>
2019-12-21 23:55:23 +0100

    form html, css, js
    improved progressbar
    added css styling
    imporoved html by adding min,max,placeholder,steps attributes

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ getData()
     .then(csvRows => transform.createHousehold(csvRows))
     .then(data => console.log(data))
     .catch(err => console.log(err))
+
+
+
+//progress-bar
+const allInputs = d3.selectAll('#uw_situatie input, #uw_situatie select')._groups[0]
+
+document.querySelectorAll('#uw_situatie input, #uw_situatie select').forEach(input => input.addEventListener('input', function () {
+    let inputsWithValue = 0
+
+    allInputs.forEach(input => {
+        if (input.tagName === 'SELECT' && input.value != '' || input.type === 'radio' && input.checked === true || input.type !== 'radio' && input.tagName !== 'SELECT' && input.value != '') {
+            inputsWithValue++
+        }
+        // else if (input.type === 'radio' && input.checked === true) {
+        //     inputsWithValue++
+        // } else if (input.type !== 'radio' && input.tagName !== 'SELECT' && input.value != '') {
+        //     inputsWithValue++
+        // }
+    })
+    let uniqueInputs = allInputs.length - 3, // '-3' is to prevent the radio buttons of taking up double-spaces
+        progression = document.querySelector('#uw_situatie #progression').parentElement.getBoundingClientRect().width / uniqueInputs * inputsWithValue //
+    document.querySelector('#uw_situatie #progression').style.paddingRight = progression + "px"
+}))
