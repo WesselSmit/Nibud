7cc353d93715fe3ca8a419a2a66bbf196cb7b7d0
Wessel Smit <wrsmit00@gmail.com>
2019-12-23 19:52:45 +0100

    start progressive disclosure

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ getData()
 
 
 
-//progress-bar
 const allInputs = d3.selectAll('#uw_situatie input, #uw_situatie select')._groups[0]
+const allQuestionCategories = d3.selectAll('.question_category')._groups[0]
 
 document.querySelectorAll('#uw_situatie input, #uw_situatie select').forEach(input => input.addEventListener('input', function () {
-    let inputsWithValue = 0
+    //progress-bar
+    let inputsWithValue = 0,
+        numberOfTotalRadio = 0
 
     allInputs.forEach(input => {
         if (input.tagName === 'SELECT' && input.value != '' ||
@@ -21,8 +23,57 @@ document.querySelectorAll('#uw_situatie input, #uw_situatie select').forEach(inp
             input.type !== 'radio' && input.tagName !== 'SELECT' && input.value != '') {
             inputsWithValue++
         }
+        if (input.type === 'radio') {
+            numberOfTotalRadio++
+        }
     })
-    let uniqueInputs = allInputs.length - 3, // '-3' is to prevent the radio buttons of taking up double-spaces
+    let uniqueInputs = allInputs.length - (numberOfTotalRadio / 2), // '-3' is to prevent the radio buttons of taking up double-spaces
         progression = document.querySelector('#uw_situatie #progression').parentElement.getBoundingClientRect().width / uniqueInputs * inputsWithValue //
     document.querySelector('#uw_situatie #progression').style.paddingRight = progression + "px"
+
+
+    //answer indicator
+    // TODO: let op onderstaande notities:
+    // ! deze methode houd geen rekening met de verschillende dynamische inputs
+    // ! alle mogelijke inputs binnen de huidige fieldset worden opgehaald
+    // ! dit betekent dat alle dynamische inputs (alle inputs die afhangen van de antwoorden van de gebruiker) moeten dynamisch gemaakt worden (d3 of JS)
+    // ! als deze dynamisch aangemaakt worden dan werkt deze functie, als je dit niet doet dan moet je deze functie herschrijven
+    let currentEl = this
+    while (currentEl.classList.contains('question_category') != true) {
+        currentEl = currentEl.parentElement
+    }
+
+    const allCurrentInputs = currentEl.querySelectorAll('input, select')
+    let answeredQuestions = 0,
+        numberOfRadio = 0
+
+    for (const currentInput of allCurrentInputs) {
+        if (currentInput.type === 'radio' && currentInput.checked === true || currentInput.type !== 'radio' && currentInput.value != '') {
+            answeredQuestions++
+        }
+        if (currentInput.type === 'radio') {
+            numberOfRadio++
+        }
+    }
+
+    if (answeredQuestions === allCurrentInputs.length - (numberOfRadio / 2)) {
+        currentEl.querySelector('div>span').classList.add('hasAnswer')
+    } else {
+        currentEl.querySelector('div>span').classList.remove('hasAnswer')
+    }
 }))
+
+//progressive disclosure
+allQuestionCategories.forEach(category => {
+    category.addEventListener('click', function () {
+        for (const question of allQuestionCategories) {
+            question.classList.remove('hide')
+            question.classList.add('hide')
+        }
+        this.classList.remove('hide')
+        for (const category of allQuestionCategories) {
+            category.querySelector('div:first-of-type>img').classList.remove('activeDropdown')
+        }
+        category.querySelector('div:first-of-type>img').classList.add('activeDropdown')
+    })
+})
