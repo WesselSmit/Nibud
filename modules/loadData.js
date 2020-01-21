3491159295f0dda2f537c3672547f71e44b1e524
Wessel Smit <wrsmit00@gmail.com>
2020-01-21 22:01:24 +0100

    data path fix

--- modules/loadData.js
+++ modules/loadData.js
@@ -5,20 +5,19 @@ export default {
 }
 
 export async function getData() {
-    const response = await fetch('../dataset.csv') //fetch data
+    const response = await fetch('./dataset.csv') //fetch data
     const csv = await response.text() //convert data to string
     return csv
 }
 
 export async function getTooltips() {
-    const response = await fetch('../tooltip.json')
+    const response = await fetch('./tooltip.json')
     const json = await response.json()
     return json.tooltips[0]
 }
 
 export async function getHighestExpenses() {
-    const response = await fetch('../bespaartips.json')
+    const response = await fetch('./bespaartips.json')
     const json = await response.json()
     return json.bespaartips[0]
 }
-
